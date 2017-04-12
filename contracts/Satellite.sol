66bfafd9ed4acd76111e50c20ec43d7aa0c2b4f2
Travis Jacobs <traviswjacobs@gmail.com>
2017-04-12 13:29:40 -0230

    Add contract UI skeletons

--- contracts/Satellite.sol
+++ contracts/Satellite.sol
@@ -1,6 +1,29 @@
 pragma solidity 0.4.8;
 
 contract Satellite {
-    //CONSTRUCTOR
+
+    struct Module {
+        string name;
+        string url;
+        int score;
+    }
+
+    // CONSTRUCTOR
     function Satellite(){}
+
+    // USER INTERFACE
+    function registerModule (string name, string url) public {
+        //pre:  no module exists with this name or url
+        //post: module with this name and url exists
+    }
+
+    function voteOnModule (string name, bool goodModule) public {
+        //pre:  module with this name exists; sender is not module's owner
+        //post: module with this name changes its score by +|- 1
+    }
+
+    function removeModule(string name) public {
+        //pre:  module with this name exists; sender is module's owner
+        //post: no module with this name exists
+    }
 }
