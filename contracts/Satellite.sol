87ef8da3330d3ad498618746bdc36b2e1d7b59ec
Travis Jacobs <traviswjacobs@gmail.com>
2017-04-12 14:03:45 -0230

    Add modifiers
    - update gitignore

--- contracts/Satellite.sol
+++ contracts/Satellite.sol
@@ -2,27 +2,51 @@ pragma solidity 0.4.8;
 
 contract Satellite {
 
+    string[] moduleNames;
+    mapping (string => Module) moduleRegistry;
+
     struct Module {
-        string name;
+        address owner;
+        string moduleName;
         string url;
         int score;
+        bool exists;
+    }
+
+    // MODIFIERS
+    modifier moduleExists (string name) {
+        if(moduleRegistry[name].exists) throw;
+        _;
+    }
+
+    modifier moduleDoesNotExist (string name) {
+        if(moduleRegistry[name].exists) throw;
+        _;
+    }
+
+    modifier senderOwnsModule (string name) {
+        if(moduleRegistry[name].owner != msg.sender) throw;
+        _;
     }
 
     // CONSTRUCTOR
     function Satellite(){}
 
     // USER INTERFACE
-    function registerModule (string name, string url) public {
+    function registerModule (string name, string url) public
+    moduleDoesNotExist(name) {
         //pre:  no module exists with this name or url
         //post: module with this name and url exists
     }
 
-    function voteOnModule (string name, bool goodModule) public {
+    function voteOnModule (string name, bool goodModule) public
+    moduleExists(name) {
         //pre:  module with this name exists; sender is not module's owner
         //post: module with this name changes its score by +|- 1
     }
 
-    function removeModule(string name) public {
+    function removeModule(string name) public
+    moduleExists(name) senderOwnsModule(name) {
         //pre:  module with this name exists; sender is module's owner
         //post: no module with this name exists
     }
