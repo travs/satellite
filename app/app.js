2da510a6c6e029dbc12e4619684d8f5f99bc9c15
Travis Jacobs <traviswjacobs@gmail.com>
2017-04-19 23:45:38 -0230

    Add tests for proof-of-email
    - checks that we can make a couple basic function calls, and trigger
      events
    - Arrange responses better in app.js

--- app/app.js
+++ app/app.js
@@ -1,41 +1,56 @@
-const Web3 = require('web3');
 const contract = require('truffle-contract');
-const satellite_artifacts = require('../build/contracts/Satellite.json');
-const email_artifacts = require('../build/contracts/ProofOfEmail.json');
-const sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+const sg = require('../node_modules/sendgrid')(process.env.SENDGRID_API_KEY);
+var helper = require('../node_modules/sendgrid').mail;
+var express = require('../node_modules/express');
+const Sentencer = require('../node_modules/sentencer');
+const ProofOfEmail = artifacts.require('./ProofOfEmail.sol');
 var helper = require('sendgrid').mail;
-var express = require('express');
-const Sentencer = require('sentencer');
 
-let provider = new Web3.providers.HttpProvider('http://localhost:8545');
-let web3 = new Web3(provider);
+// EMAIL API
+function sendCodeEmail (email, code) {
+  var from_email = new helper.Email('verify@melonport.com');
+  var to_email = new helper.Email(email);
+  var subject = 'Melonport Verification Request';
+  var content = new helper.Content('text/plain',
+    `Please enter this code at the contract:  ${code}`
+  );
+  var mail = new helper.Mail(from_email, subject, to_email, content);
+
+  var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+  return sg.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: mail.toJSON()
+  });
+}
 
 // ROUTING
-let server = express();
-server.post('/address/:address/email/:email', function (req, res) {
+let app = express();
+app.post('/address/:address/email/:email', function (req, res) {
   // generate the code and token
   // send token to contract and send code to client
   var code = Sentencer.make('{{ adjective }} {{ adjective }} {{ nouns }}');
-  var token = web3.sha3(code);
-  emailVerifyInstance.puzzle(
-    req.params.address, web3.sha3(token, {encoding: 'hex'}),
-    web3.sha3(req.params.email), {from: web3.eth.accounts[0]}
-  )
+  ProofOfEmail.deployed()
+  .then(instance => {
+    var token = web3.sha3(code);
+    return instance.puzzle(
+      req.params.address, web3.sha3(token, {encoding: 'hex'}),
+      web3.sha3(req.params.email)
+    );
+  })
   .then(() => {
-    sendCodeEmail(req.params.email, code);
-    res.send(`Verification email sent to ${req.params.email}`);
+    var request = sendCodeEmail(req.params.email, code);
+    sg.API(request, (err, response) => {
+      if(!err)
+        res.status(200).send(`Verification email sent to ${req.params.email}`);
+      else
+        res.status(400).send('Failure sending mail');
+    });
   })
 })
 
-// RUN SERVER
-let emailVerifyInstance;
-let ProofOfEmail = contract(email_artifacts);
-ProofOfEmail.setProvider(provider);
-ProofOfEmail.deployed()
-.then(instance => emailVerifyInstance = instance)
-.then(() => requestedEvent = emailVerifyInstance.Requested())
-.then(() => {
-  server.listen(3002, function () {
-    console.log('Server started')
-  })
-})
+var server = app.listen(3002, function () {
+  // console.log('Server started');
+});
+
+module.exports = server;
---
+++ test/proof-of-email-unit-tests.js
@@ -0,0 +1,34 @@
+const ProofOfEmail = artifacts.require('./ProofOfEmail.sol');
+
+let instance;
+let email = 'nycacevi@inboxbear.com';
+let code = 'abc123';
+
+describe('Proof of email contract', () => {
+contract('ProofOfEmail', accounts => {
+  let address = accounts[3];
+
+  before('Preparation', () => {
+    ProofOfEmail.deployed()
+    .then(res => instance = res)
+  });
+
+  it('Correctly makes a puzzle function call', () => {
+    token = web3.sha3(code);
+    assert.doesNotThrow(() => {
+      return instance.puzzle(
+        address, web3.sha3(token, {encoding: 'hex'}),
+        web3.sha3(email)
+      )
+      .then(res => assert.equal(res.logs[0].event, 'Puzzled'))
+    })
+  })
+  it('Correctly makes confirm function call', () => {
+    assert.doesNotThrow(() => {
+      return instance.confirm(token, {from: address})
+      .then(res => assert.equal(res.logs[0].event, 'Confirmed'))
+    })
+  })
+
+})
+})
